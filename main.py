4a7f60704f6c00ba9a84bef43fb7541ce2f647de
DemetriceW20 <demetrice_williams@hotmail.com>
2021-03-22 13:14:37 -0800

    Changed name of players

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ if __name__ == '__main__':
     rpsls.welcome_message()
     rpsls.rules()
     rpsls.select_mode()
-
     cpu = CPU()
-    player1 = Humans('Will')
-    player2 = Humans('John')
-
+    player1 = Humans('John')
+    player2 = Humans('Jane')
     rpsls.start_game(player1, player2, cpu)
     rpsls.end_game()
 
